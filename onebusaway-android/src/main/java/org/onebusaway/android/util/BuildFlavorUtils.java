32873a59fd22188b4931233aa68a41696ed09697
mazurikian <mazurikian@protonmail.com>
2026-06-21 09:08:23 -0300

    changes

--- onebusaway-android/src/main/java/org/onebusaway/android/util/BuildFlavorUtils.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/util/BuildFlavorUtils.java
@@ -31,8 +31,6 @@ import org.onebusaway.android.app.Application;
 public class BuildFlavorUtils {
 
     public static final String OBA_FLAVOR_BRAND = "oba";
-    
-    public static final String AGENCYY_FLAVOR_BRAND = "agencyY";
 
     public static final int ARRIVAL_INFO_STYLE_A = 0; // Original OBA style
 
